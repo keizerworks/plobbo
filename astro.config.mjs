bc09d60f542b54611204ca1cfaabfc353af50ce3
Sudarsh1010 <sudarshjain1010@gmail.com>
2024-12-28 01:59:48 +0530

    add node runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import db from '@astrojs/db';
+import db from "@astrojs/db";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), tailwind(), react()]
+  integrations: [db(), tailwind(), react()],
+  adapter: node({ mode: "standalone" }),
 });
+
