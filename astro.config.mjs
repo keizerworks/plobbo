772c95deb0a12c899f84e49f6e6aaad086ea2789
Sudarsh1010 <sudarshjain1010@gmail.com>
2024-12-28 02:45:02 +0530

    add linter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
 import db from "@astrojs/db";
-
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), tailwind(), react()],
   adapter: node({ mode: "standalone" }),
 });
-
