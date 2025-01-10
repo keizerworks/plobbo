f3bc8830bcdbc9a9e3edb458d0e0016e7f51b6c1
Your Name <you@example.com>
2025-01-10 18:09:13 +0800

    add user lis in dashboard
    add org list in dashboard

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,9 @@
 import { blogs } from "./dashboard/blogs";
+import { users } from "./dashboard/users";
+import { organizations } from "./dashboard/organization";
 
 export const server = {
   blogs,
+  users,
+  organizations,
 };
