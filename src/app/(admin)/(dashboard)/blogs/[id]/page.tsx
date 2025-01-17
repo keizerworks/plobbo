fa02950324fe136e825b0edd5bc12308fbc756a8
Sudarsh1010 <sudarshjain1010@gmail.com>
2025-01-18 03:41:46 +0530

    import prosemirror css to editor page, fix p tag height

--- src/app/(admin)/(dashboard)/blogs/[id]/page.tsx
+++ src/app/(admin)/(dashboard)/blogs/[id]/page.tsx
@@ -1,9 +1,11 @@
+import "styles/prosemirror.css";
+
 import NovelEditor from "components/editor/wrapper";
 
 export default function Page() {
   return (
     <main className="mx-auto flex size-full max-w-6xl flex-col gap-y-2 p-4">
-      <div className="relative flex size-full items-center justify-center rounded-xl border p-4 transition-all animate-in fade-in-0">
+      <div className="relative flex size-full items-center justify-center rounded-xl border transition-all animate-in fade-in-0">
         <NovelEditor />
       </div>
     </main>
