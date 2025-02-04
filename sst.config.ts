4be611aa6f271a1bb32e269fc34594a835a618b5
Sudarsh1010 <sudarshjain1010@gmail.com>
2025-02-04 16:36:19 +0530

    rename app

--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "keizer-blogs",
+      name: "plobbo",
       removal: input.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input.stage),
       home: "aws",
