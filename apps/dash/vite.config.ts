26ded4e4627227bbb68f0f162f9d2740ef6a1ff6
Sudarsh1010 <sudarshjain1010@gmail.com>
2025-02-20 12:53:19 +0530

    fix dash build issue

--- apps/dash/vite.config.ts
+++ apps/dash/vite.config.ts
@@ -1,5 +1,4 @@
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
@@ -7,7 +6,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     TanStackRouterVite({
       generatedRouteTree: "./src/route-tree.gen.ts",
       routesDirectory: "./src/routes",
