d40fd253aada0a74127cd32ce49190778118c3d9
professorabhay <abhayetwup123@gmail.com>
2025-02-25 21:25:43 +0530

    Legal Terms and Policies added
    Signed-off-by: professorabhay <abhayetwup123@gmail.com>

---
+++ apps/www/src/app/terms/page.tsx
@@ -0,0 +1,11 @@
+import { TermsOfService } from "../../components/legal/legal-terms"
+import { LegalLayout } from "../../components/legal/legal-layout"
+
+export default function TermsOfServicePage() {
+  return (
+    <LegalLayout>
+      <TermsOfService />
+    </LegalLayout>
+  )
+}
+
